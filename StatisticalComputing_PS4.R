8f978e4c4d48ab232399f71a17a27cbe3ddc0b03
Noah Bardash <noah.bardash@wustl.edu>
2018-02-21 19:41:17 -0600

    completed part 2, tested debugging feature with mistakes

--- StatisticalComputing_PS4.R
+++ StatisticalComputing_PS4.R
@@ -31,7 +31,7 @@ setClass(Class="door",
 setValidity("door", function(object){
   if(object@chosenDoor < 1 | object@chosenDoor > 3){return("invalid chosen door")}
   if(object@carDoor < 1 | object@carDoor > 3){return("invalid car door")}
-  if(object@switch != TRUE & object@switch != FALSE){return("invalid switch entry")}
+  #if(object@switch != TRUE & object@switch != FALSE){return("invalid switch entry")}
 }
 )
 
@@ -43,34 +43,49 @@ setMethod("initialize", "door", function(.Object, ...) {
 
 game1 <- new("door", chosenDoor=as.integer(1), carDoor=as.integer(3), switch=FALSE)
 game2 <- new("door", chosenDoor=as.integer(1), carDoor=as.integer(1), switch=FALSE)
+game3 <- new("door", chosenDoor=as.integer(1), carDoor=as.integer(1), switch=TRUE)
 
 
+sample(3,1)
 
 # 2
+
+winner <- NULL
 
 setGeneric("PlayGame", function(object="door") {
   standardGeneric("PlayGame")
 })
 setMethod("PlayGame", "door", function(object){
-  firstChosenDoor = sample(1:3, 1)
-  object@carDoor = sample(1:3, 1)
+  firstChosenDoor = sample(3, 1)
+  object@carDoor = sample(3, 1)
   if(object@switch == FALSE){
     object@chosenDoor = firstChosenDoor
-    if(object@chosenDoor == object@carDoor){winner = TRUE}
-    else{winner = FALSE}
   }
   else{
-    
+    removedDoor = 0
+    while((removedDoor != firstChosenDoor) & (removedDoor != object@carDoor)){
+      removedDoor = sample(3, 1)
+    }
+    if(sample(2, 1) == 1){
+      object@chosenDoor = firstChosenDoor
+    }
+    else{
+      lastDoor = 0
+      while((lastDoor != firstChosenDoor) & (lastDoor != removedDoor))
+        lastDoor = sample(3, 1)
+    }
   }
-  if(winner == TRUE){return("Congrats! You win.")}
+  if(object@chosenDoor == object@carDoor){winner <<- TRUE}
+  else{winner <<- FALSE}
+  if(winner == TRUEreturn("Congrats! You win.")}
   else{return("Better luck next time!")}
 })
-
-
+#added mistakes to debug
+  
+debug(PlayGame)
 PlayGame(game2)
+PlayGame(game3)
 
-#setValidity("door", function(game){
-# winner = FALSE
-# 
-#}
-#)
+# Simulation
+
+
